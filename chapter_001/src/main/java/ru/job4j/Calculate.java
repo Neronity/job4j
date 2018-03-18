5508e91d89a23deaea9619e7de5941b13fe52a4a
Neronity <andrejjsigachev@gmail.com>
2018-03-18 12:11:33 +0300

    1.3. JavaDoc. Code convention.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j;
 
+/**
+* Calculate.
+* @author Andrey Sigachev (andrejjsigachev@gmail.com)
+*/
 public class Calculate {
 
+	/**
+	* Main.
+	* @param args - args.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for clculate task.
+* @author Andrey Sigachev (andrejjsigachev@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
