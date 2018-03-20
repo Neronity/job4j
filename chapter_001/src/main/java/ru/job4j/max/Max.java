e6d93a1603c316ee7995a4990e7f9402033960c4
Neronity <andrejjsigachev@gmail.com>
2018-03-20 20:21:01 +0300

    4.4. Максимум из 3 чисел #190

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -13,4 +13,15 @@ public class Max {
 	public int max(int firstNum, int secondNum) {
 		return firstNum > secondNum ? firstNum : secondNum;
 	}
+
+	/**
+	 * Наибольшее из 3 чисел
+	 * @param firstNum 1 число
+	 * @param secondNum 2 число
+	 * @param thirdNum 3 число
+	 * @return наибольшее из 3 чисел
+	 */
+	public int max(int firstNum, int secondNum, int thirdNum) {
+		return max(max(firstNum, secondNum), max(secondNum, thirdNum));
+	}
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -10,4 +10,10 @@ public class MaxTest {
 		Max max = new Max();
 		assertThat(max.max(1, 2), is(2));
 	}
+
+	@Test
+	public void WhenOneTwoAndThreeThenThree() {
+		Max max = new Max();
+		assertThat(max.max(1, 2, 3), is(3));
+	}
 }
