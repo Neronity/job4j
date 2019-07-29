3fbafdee19a644981302e126d351fd8191a9af6e
Andrey <andrejjsigachev@gmail.com>
2019-07-29 07:02:37 +0300

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class SortUser {
 
-    public  Set<User> sort(List<User> list) {
+    public Set<User> sort(List<User> list) {
         Set<User> result = new TreeSet<>(new Comparator<User>() {
             @Override
             public int compare(User o1, User o2) {
@@ -15,4 +15,25 @@ public class SortUser {
         return result;
     }
 
+
+    public List<User> sortNameLength(List<User> list) {
+        Collections.sort(list, (new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.name.length() - o2.name.length();
+            }
+        }));
+        return list;
+    }
+
+    public List<User> sortByAllFields(List<User> list) {
+        Collections.sort(list, (new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int result = o1.name.compareTo(o2.name);
+                return result != 0 ? result : o1.age - o2.age;
+            }
+        }));
+        return list;
+    }
 }
--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -1,4 +1,5 @@
 package ru.job4j.sort;
+
 import org.junit.Test;
 
 import java.util.*;
@@ -29,8 +30,57 @@ public class SortUserTest {
                         u2,
                         u3,
                         u1
-        ));
+                ));
         assertThat(result, is(expect));
     }
 
+    @Test
+    public void WhenSortByNameLengthThenSortedList() {
+        User u1 = new User(4, "Ирa");
+        User u2 = new User(2, "Ирина");
+        User u3 = new User(3, "Филипп");
+        User u4 = new User(1, "Добромир");
+        List<User> list = new ArrayList<>(Arrays.asList(
+                u4,
+                u3,
+                u2,
+                u1
+        ));
+        List<User> result = new SortUser().sortNameLength(list);
+        List<User> expect = new ArrayList<>(
+                Arrays.asList(
+                        u1,
+                        u2,
+                        u3,
+                        u4
+                )
+        );
+        assertThat(result, is(expect));
+    }
+
+
+    @Test
+    public void WhenSameNameThenSortByAge() {
+        User u1 = new User(4, "Ирина");
+        User u2 = new User(2, "Ирина");
+        User u3 = new User(3, "Филипп");
+        User u4 = new User(1, "Филипп");
+        List<User> list = new ArrayList<>(Arrays.asList(
+                u1,
+                u3,
+                u2,
+                u4
+        ));
+
+        List<User> result = new SortUser().sortByAllFields(list);
+        List<User> expect = new ArrayList<>(
+                Arrays.asList(
+                        u2,
+                        u1,
+                        u4,
+                        u3
+                )
+        );
+        assertThat(result, is(expect));
+    }
 }
