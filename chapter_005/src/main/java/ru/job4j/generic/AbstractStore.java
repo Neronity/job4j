5fb2339f2b98cd21c6774b7cb24c0e1aa2a3ee39
Andrey <andrejjsigachev@gmail.com>
2019-08-02 14:00:59 +0300

    5.2.2. Реализовать Store<T extends Base>[#164123]

---
+++ chapter_005/src/main/java/ru/job4j/generic/AbstractStore.java
@@ -0,0 +1,63 @@
+package ru.job4j.generic;
+
+import java.util.Iterator;
+
+public class AbstractStore<T extends Base> implements Store<T> {
+
+    private SimpleArray<T> sa = new SimpleArray<>(50);
+
+    @Override
+    public void add(T model) {
+        sa.add(model);
+    }
+
+    @Override
+    public boolean replace(String id, T model) {
+        boolean result = true;
+        try {
+            sa.set(indexOf(id), model);
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            result = false;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        boolean result = true;
+        try {
+            sa.remove(indexOf(id));
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            result = false;
+        }
+        return result;
+    }
+
+
+    public int indexOf(String id) {
+        Iterator<T> it = sa.iterator();
+        int index = 0;
+        boolean success = false;
+        while (it.hasNext()) {
+            if (it.next().getId().equals(id)) {
+                success = true;
+                break;
+            }
+            index++;
+        }
+        return success ? index : -1;
+    }
+
+    @Override
+    public T findById(String id) {
+        Iterator<T> it = sa.iterator();
+        T result = null;
+        while (it.hasNext()) {
+            T base = it.next();
+            if (base.getId().equals(id)) {
+                result = base;
+            }
+        }
+        return result;
+    }
+}
