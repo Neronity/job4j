dc3f8c5ff08621cbc45ecc9804416ad3c11c0a6f
Andrey <andrejjsigachev@gmail.com>
2019-08-09 14:57:24 +0300

    1. Читаем файл конфигурации[#164135]

---
+++ chapter_005/src/test/java/ru/job4j/io/config/ConfigTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.io.config;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ConfigTest {
+    private Config conf = new Config("/Users/clutterfunk/Documents/GitHub" +
+            "/job4j/chapter_005/src/test/java/ru/job4j/io/config/app.properties");
+
+    @Test
+    public void whenLoadFileThenMapFilled() {
+        conf.load();
+        assertThat(conf.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
+        assertThat(conf.value("hibernate.connection.url"), is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
+        assertThat(conf.value("hibernate.connection.driver_class"), is("org.postgresql.Driver"));
+        assertThat(conf.value("hibernate.connection.username"), is("postgres"));
+        assertThat(conf.value("hibernate.connection.password"), is("password"));
+        assertThat(conf.valuesSize(), is(5));
+    }
+}
