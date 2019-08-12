f3cf484dade284455d365577a5ac457dcc631503
Andrey <andrejjsigachev@gmail.com>
2019-08-12 23:13:19 +0300

    more test fixes

--- chapter_005/src/test/java/ru/job4j/io/search/SearchTest.java
+++ chapter_005/src/test/java/ru/job4j/io/search/SearchTest.java
@@ -13,22 +13,22 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class SearchTest {
-    private String tmpDir = "src/test/java/ru/job4j/io/search";
+    private String tmpDir = "src/test/java/ru/job4j/io/search/";
     private Search s = new Search();
 
     @Test
     public void whenThen() {
-        new File(tmpDir + "/dir1").mkdirs();
-        new File(tmpDir + "/dir1/dir2").mkdirs();
+        new File(tmpDir + "dir1").mkdirs();
+        new File(tmpDir + "dir1/dir2").mkdirs();
         try {
-            new File(tmpDir + "/dir1/file1.scv").createNewFile();
-            new File(tmpDir + "/dir1/file1.txt").createNewFile();
-            new File(tmpDir + "/dir1/dir2/file2.txt").createNewFile();
-            new File(tmpDir + "/dir1/dir2/file2.cs").createNewFile();
+            new File(tmpDir + "dir1/file1.scv").createNewFile();
+            new File(tmpDir + "dir1/file1.txt").createNewFile();
+            new File(tmpDir + "dir1/dir2/file2.txt").createNewFile();
+            new File(tmpDir + "dir1/dir2/file2.cs").createNewFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        List<File> result = s.files(tmpDir + "/dir1", Arrays.asList("txt", "cs"));
+        List<File> result = s.files(tmpDir + "dir1", Arrays.asList("txt", "cs"));
         assertThat(result.size(), is(3));
         assertThat(result.stream().map(File::getName).collect(Collectors.toList()),
                 is(Arrays.asList("file1.txt", "file2.txt", "file2.cs")));
