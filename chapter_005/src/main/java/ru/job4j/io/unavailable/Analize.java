4104866c0288577fffb4bf9d2d6644980c50be9f
Andrey <andrejjsigachev@gmail.com>
2019-08-12 17:53:21 +0300

    2. Анализ доступности сервера.[#164136]

--- chapter_005/src/main/java/ru/job4j/io/unavailable/Analize.java
+++ chapter_005/src/main/java/ru/job4j/io/unavailable/Analize.java
@@ -1,28 +1,40 @@
 package ru.job4j.io.unavailable;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringJoiner;
 
 public class Analize {
 
+    private static final StringBuilder SB = new StringBuilder();
+    private static final String LN = System.lineSeparator();
+
     public void unavailable(String source, String target) {
-        final StringBuilder sb = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(source));
-        PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
-            reader.lines().map(line -> line.split(" ")).forEach(e -> {
-                if (sb.length() == 0) {
-                    if (e[0].equals("400") || e[0].equals("500")) {
-                        sb.append(e[1]).append(";");
-                    }
-                } else {
-                    if (e[0].equals("200") || e[0].equals("300")) {
-                        sb.append(e[1]);
-                        writer.println(sb);
-                        sb.delete(0, sb.length());
-                    }
-                }
-            });
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+            reader.lines().map(line -> line.split(" ")).forEach(this::parseLogs);
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+        writeParsedLogs(target);
+    }
+
+    private void parseLogs(String[] s) {
+        if (SB.length() == 0 || SB.charAt(SB.length() - 1) != ';') {
+            if (s[0].equals("400") || s[0].equals("500")) {
+                SB.append(s[1]).append(";");
+            }
+        } else {
+            if (s[0].equals("200") || s[0].equals("300")) {
+                SB.append(s[1]).append(LN);
+            }
+        }
+    }
+
+    private void writeParsedLogs(String target) {
+        try (PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
+            writer.write(SB.toString());
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
